568c4823f203dfc1690a3f8f9e0f13a1617358ba
Courtney S. <bbcourt@gmail.com>
2012-05-24 15:27:12 -0500

    Cleaned up the code and made minor changes

--- CameraMove.py
+++ CameraMove.py
@@ -2,24 +2,19 @@ import viz
 import vizact
 import vizshape
 import math
-viz.go(viz.PROMPT)
+viz.go()
 
 
-tracker = viz.add('intersense.dls')
-#viz.translate(viz.HEAD_POS,0,-1.8,0)
-#viz.eyeheight(1.8);
+''' *************************** Set Up Scene **************************** '''
 ground = viz.add('tut_ground.wrl')
+sky = viz.clearcolor(viz.SKYBLUE)
 #panorama = viz.add('panorama.ive')
-#scaffold = viz.add('scaffold.ive')
+#grid = vizshape.addGrid()
+''' *********************** End of Scene set up ************************* '''
 
-##viz.clearcolor(viz.SKYBLUE)
-#piazza = viz.addChild('piazza.osgb')
 
-#vizshape.addGrid()
-viz.collision(viz.ON)
-
-#Kinect Tracker object ID's
-#myHead = vrpn.addTracker( 'Tracker0@localhost', HEAD).
+''' *************************** KINECT CODE ***************************** '''
+#myHead = vrpn.addTracker( 'Tracker0@localhost', HEAD)
 HEAD = 0
 NECK = 1
 TORSO = 2
@@ -45,96 +40,115 @@ RIGHTKNEE = 21
 RIGHTANKLE = 22
 RIGHTFOOT = 23
 
-#store trackers, links, and vizshape objects
+# Store trackers, links, and vizshape objects
 trackers = []
 links = []
 shapes = []
 
-#start vrpn
+# Start vrpn
 vrpn = viz.addExtension('vrpn7.dle')
 
-viz.MainView.setPosition(-1,8,1)
-
-lastX = 0
-lastZ = 3
-#now add all trackers and link a shape to it
+# Now add all trackers and link a shape to it
 for i in range(0,24):
-	t = vrpn.addTracker( 'Tracker0@10.20.31.37',i )
-	
-	s = vizshape.addSphere(radius=0)
-	
-	if i == 2: #for the torso:
-		s.color(viz.RED)
-		#display an arrow pointing away from the avatar
-		global arrow
-		arrow = vizshape.addCylinder(1.5,0.05,topRadius=0)
-		arrow.parent(s)
-		arrow.setPosition([0,0,-0.75])
-		arrow.setEuler([0,-90,0])
-		
-	elif i in [5,11,16,20]:
-		s.color(viz.BLUE)
-		
+	t = vrpn.addTracker('Tracker0@localhost', i)
+	s = vizshape.addSphere(radius = 0.1)
 	l = viz.link(t,s)
 	trackers.append(t)
 	links.append(l)
 	shapes.append(s)
-	
-RF = vrpn.addTracker('Tracker0@10.20.31.37', RIGHTFOOT)
-LF = vrpn.addTracker('Tracker0@10.20.31.37', LEFTFOOT)
-RK = vrpn.addTracker('Tracker0@10.20.31.37', RIGHTKNEE)
-LK = vrpn.addTracker('Tracker0@10.20.31.37', LEFTKNEE)
-RH = vrpn.addTracker('Tracker0@10.20.31.37', RIGHTHIP)
-LH = vrpn.addTracker('Tracker0@10.20.31.37', LEFTHIP)
-LS = vrpn.addTracker('Tracker0@10.20.31.37', LEFTSHOULDER)
-RS = vrpn.addTracker('Tracker0@10.20.31.37', RIGHTSHOULDER)
-Torso = vrpn.addTracker('Tracker0@10.20.31.37', TORSO)
 
-initialStep = 0
+	if i == HEAD:
+		s.color(viz.GREEN)
+	elif i == TORSO:
+		s.color(viz.RED)
+		# Add an arrow pointing from the torso in the direction the body is facing
+		arrow = vizshape.addCylinder(1,0.05,topRadius=0)
+		arrow.parent(s)
+		arrow.setEuler([0,-90,0])
+		arrow.setPosition([0,0,-0.5])
+		arrow.color(viz.RED)
+		
+	elif i in [LEFTSHOULDER,RIGHTSHOULDER,LEFTHIP,RIGHTHIP]:
+		s.color(viz.BLUE)
+
+# Trackers for specified bodyparts
+RF = vrpn.addTracker('Tracker0@localhost', RIGHTFOOT)
+LF = vrpn.addTracker('Tracker0@localhost', LEFTFOOT)
+RK = vrpn.addTracker('Tracker0@localhost', RIGHTKNEE)
+LK = vrpn.addTracker('Tracker0@localhost', LEFTKNEE)
+RH = vrpn.addTracker('Tracker0@localhost', RIGHTHIP)
+LH = vrpn.addTracker('Tracker0@localhost', LEFTHIP)
+RS = vrpn.addTracker('Tracker0@localhost', RIGHTSHOULDER)
+LS = vrpn.addTracker('Tracker0@localhost', LEFTSHOULDER)
+Torso = vrpn.addTracker('Tracker0@localhost', TORSO)
+''' ************************ End of KINECT CODE ************************* '''
+
+
+''' ************************* Initializations *************************** '''
+tracker = viz.add('intersense.dls')
+#viz.translate(viz.HEAD_POS,0,-1.8,0)
+#viz.eyeheight(1.8);
+
+viz.MainView.setPosition(0,1.8,-3)
 prevStep = "DOWN"
-yaw = 0
+initialStep = 0
 checkYaw = 0
+yaw = 0
+''' ********************* End of Initializations ************************ '''
+
 
 def crossProduct(a,b):
 	c = [a[1]*b[2] - a[2]*b[1],
 	     a[2]*b[0] - a[0]*b[2],
 	     a[0]*b[1] - a[1]*b[0]]
-	
 	return c
 
+
 def calcNormal():
-	a1 = [RS.getPosition()[0] - LS.getPosition()[0], RS.getPosition()[1] - LS.getPosition()[1], RS.getPosition()[2] - LS.getPosition()[2]]
-	b1 = [RH.getPosition()[0] - RS.getPosition()[0], RH.getPosition()[1] - RS.getPosition()[1], RH.getPosition()[2] - RS.getPosition()[2]]
+	a1 = [RS.getPosition()[0] - LS.getPosition()[0],
+		  RS.getPosition()[1] - LS.getPosition()[1],
+		  RS.getPosition()[2] - LS.getPosition()[2]]
+	b1 = [RH.getPosition()[0] - RS.getPosition()[0],
+		  RH.getPosition()[1] - RS.getPosition()[1],
+		  RH.getPosition()[2] - RS.getPosition()[2]]
 	c1 = crossProduct(a1,b1)
 	
-	a2 = [LH.getPosition()[0] - RH.getPosition()[0], LH.getPosition()[1] - RH.getPosition()[1], LH.getPosition()[2] - RH.getPosition()[2]]
-	b2 = [LS.getPosition()[0] - LH.getPosition()[0], LS.getPosition()[1] - LH.getPosition()[1], LS.getPosition()[2] - LH.getPosition()[2]]
+	a2 = [LH.getPosition()[0] - RH.getPosition()[0],
+		  LH.getPosition()[1] - RH.getPosition()[1],
+		  LH.getPosition()[2] - RH.getPosition()[2]]
+	b2 = [LS.getPosition()[0] - LH.getPosition()[0],
+		  LS.getPosition()[1] - LH.getPosition()[1],
+		  LS.getPosition()[2] - LH.getPosition()[2]]
 	c2 = crossProduct(a2,b2)
 	
 	normal = [(c1[0] + c2[0])/2, -(c1[2] + c2[2])/2]
 	normalMag = math.hypot(normal[0],normal[1])
+	
 	x = normal[0]/normalMag
 	z = normal[1]/normalMag
 	
 	return x,z
 
+
 def getInitial():
-	global initialStep 
+	global initialStep
 	initialKnee = ((LK.getPosition())[1] + (RK.getPosition())[1])/2
 	initialFeet = ((LF.getPosition())[1] + (RF.getPosition())[1])/2
+	
 	initialStep = (initialKnee + initialFeet) * .6
+
 
 def setDown():
 	global prevStep
 	prevStep = "DOWN"
 	viz.MainView.velocity(0,0,0)
 
+
 def step():
 	global prevStep
 	prevStep = "UP"
 	
 	x,z = calcNormal()
-	
 	viz.MainView.velocity(x, 0, z)
 	
 	x = math.sin(math.radians(-yaw)) + viz.MainView.getPosition()[0]
@@ -142,11 +156,11 @@ def step():
 		
 	#viz.MainView.lookat([x,2,z])
 	vizact.ontimer2(.9,0,setDown)
-		
+
+
 def checkStep():
 	global checkYaw, yaw
 	checkYaw = yaw
-	
 	yaw = Torso.getEuler()[0]
 	
 	LFvert = (LF.getPosition())[1]
@@ -155,19 +169,15 @@ def checkStep():
 	x = math.sin(math.radians(-yaw)) + viz.MainView.getPosition()[0]
 	z = math.cos(math.radians(yaw)) + viz.MainView.getPosition()[2]
 		
-#	viz.MainView.lookat([x,2,z])
-	
-	data = tracker.getData()
+	#viz.MainView.lookat([x,2,z])
 	
 	if prevStep == "DOWN" and (LFvert > initialStep or RFvert > initialStep):
 		step()
 	
-	viz.MainView.rotate(data[3]+90,data[4],data[5],'',viz.BODY_ORI)
-	
-	#viz.MainView.setEuler(data[3], data[4], data[5])
-	
-	
-	
+	data = tracker.getData()
+	#viz.MainView.rotate(data[3]+90,data[4],data[5],'',viz.BODY_ORI)
+	viz.MainView.setEuler(data[3], data[4], data[5])
 
-vizact.ontimer2(0.5, 0, getInitial)
+
+vizact.ontimer2(0.33, 2, getInitial)
 vizact.ontimer(1/60, checkStep)
